0d7c4db27800ba024bed1839591f112998b9cba5
Manel <mmpcp23@gmail.com>
2019-11-14 16:15:57 +0000

    Testing - V2

--- FST-MP2/run.sh
+++ FST-MP2/run.sh
@@ -3,17 +3,13 @@
 rm -f *.fst *.pdf
 
 # - create folders
-rm -r FINALtransducers
-rm -r AUXtransducers
-rm -r FINALexamples
-rm -r FINALpdf
-rm -r TXTexamples
-rm -r FSTexamples
+rm -r FINALtransducers AUXtransducers FINALexamples FINALpdf FSTexamples
+#rm -r TXTexamples
 mkdir FINALtransducers
 mkdir AUXtransducers
 mkdir FINALexamples
 mkdir FINALpdf
-mkdir TXTexamples
+#mkdir TXTexamples
 mkdir FSTexamples
 
 # - Lemma
@@ -34,37 +30,27 @@ fstcompile --isymbols=syms.txt --osymbols=syms.txt --keep_isymbols --keep_osymbo
 # a. Concantenate
 # 1 - Noun
 fstconcat AUXtransducers/lemma.fst AUXtransducers/noun.fst > FINALtransducers/lemma2noun.fst
-echo ""
 fstrmepsilon FINALtransducers/lemma2noun.fst FINALtransducers/lemma2noun.fst
-echo " "
 fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait FINALtransducers/lemma2noun.fst | dot -Tpdf > FINALpdf/lemma2noun.pdf
 
 # 2 - Adverb
 fstconcat AUXtransducers/lemma.fst AUXtransducers/adverb.fst > FINALtransducers/lemma2adverb.fst
-echo ""
 fstrmepsilon FINALtransducers/lemma2adverb.fst FINALtransducers/lemma2adverb.fst
-echo " "
 fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait FINALtransducers/lemma2adverb.fst | dot -Tpdf > FINALpdf/lemma2adverb.pdf
 
 # 3 - Verb Indicative Present
 fstconcat AUXtransducers/lemma.fst AUXtransducers/verbip.fst > FINALtransducers/lemma2verbip.fst
-echo ""
 fstrmepsilon FINALtransducers/lemma2verbip.fst FINALtransducers/lemma2verbip.fst
-echo " "
 fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait FINALtransducers/lemma2verbip.fst | dot -Tpdf > FINALpdf/lemma2verbip.pdf
 
 # 4 - Verb Indicative Past
 fstconcat AUXtransducers/lemma.fst AUXtransducers/verbis.fst > FINALtransducers/lemma2verbis.fst
-echo ""
 fstrmepsilon FINALtransducers/lemma2verbis.fst FINALtransducers/lemma2verbis.fst
-echo " "
 fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait FINALtransducers/lemma2verbis.fst | dot -Tpdf > FINALpdf/lemma2verbis.pdf
 
 # 5 - Verb Indicative Future
 fstconcat AUXtransducers/lemma.fst AUXtransducers/verbif.fst > FINALtransducers/lemma2verbif.fst
-echo ""
 fstrmepsilon FINALtransducers/lemma2verbif.fst FINALtransducers/lemma2verbif.fst
-echo " "
 fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait FINALtransducers/lemma2verbif.fst | dot -Tpdf > FINALpdf/lemma2verbif.pdf
 
 # b. Unite
@@ -77,7 +63,6 @@ fstunion AUXtransducers/verbif.fst AUXtransducers/verbips.fst > AUXtransducers/v
 
 fstconcat AUXtransducers/lemma.fst AUXtransducers/verb.fst > FINALtransducers/lemma2verb.fst
 fstrmepsilon FINALtransducers/lemma2verb.fst FINALtransducers/lemma2verb.fst
-echo " "
 fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait FINALtransducers/lemma2verb.fst | dot -Tpdf > FINALpdf/lemma2verb.pdf
 
 # 2 - Lemma to Words
@@ -89,7 +74,6 @@ fstunion AUXtransducers/verb_adverb.fst AUXtransducers/noun.fst > AUXtransducers
 
 fstconcat AUXtransducers/lemma.fst AUXtransducers/word.fst > FINALtransducers/lemma2word.fst
 fstrmepsilon FINALtransducers/lemma2word.fst FINALtransducers/lemma2word.fst
-echo " "
 fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait FINALtransducers/lemma2word.fst | dot -Tpdf > FINALpdf/lemma2word.pdf
 
 # 3 - Word to Lemma
@@ -99,26 +83,35 @@ fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait FINALtransducers/word
 
 # TESTING
 
-array=(  "cantar+V+is+1s" "barco+N+fp" "rapidamente" "cantar+V+ip+1s" "cantar+V+ip+2s" "cantar+V+ip+3s" "cantar+V+ip+1p" "cantar+V+ip+2p" "cantar+V+ip+3p" "cantar+V+is+2s" "cantar+V+is+3s" "cantar+V+is+1p" "cantar+V+is+2p" "cantar+V+is+3p"  "cantar+V+if+1s" "cantar+V+if+2s" "cantar+V+if+3s" "cantar+V+if+1p" "cantar+V+if+2p" "cantar+V+if+3p" "saltar+V+if+2p" "constantemente+ADV" "tubo"       "rema")
-array2=( "lemma2verb"     "lemma2word" "word2lemma"  "lemma2verb"     "lemma2verb"     "lemma2verb"     "lemma2verb"     "lemma2verb"     "lemma2verb"     "lemma2verb"     "lemma2verb"     "lemma2verb"     "lemma2verb"     "lemma2verb"      "lemma2verb"     "lemma2verb"     "lemma2verb"     "lemma2verb"     "lemma2verb"     "lemma2verb"     "lemma2word"     "lemma2word"         "word2lemma" "word2lemma" )
-array3=( "test1"          "test2"      "test3"       "test4"          "test5"          "test6"          "test7"          "test8"          "test9"          "test10"         "test11"         "test12"         "test13"         "test14"          "test15"         "test16"         "test17"         "test18"         "test20"         "test21"         "test22"         "test23"             "test24"     "test25"     )
-
+#array with test mames
+array=(  "cantar+V+ip+1s" "canto" "cantar+V+ip+2s" "cantas" "cantar+V+ip+3s" "canta" "cantar+V+ip+1p" "cantais" "cantar+V+ip+2p" "cantamos" "cantar+V+ip+3p" "cantam" "cantar+V+is+1s" "cantei" "cantar+V+is+2s" "cantaste" "cantar+V+is+3s" "cantou" "cantar+V+is+1p" "cantastes" "cantar+V+is+2p" "cantaram" "cantar+V+is+3p" "cantamos" "cantar+V+if+1s" "cantarei" "cantar+V+if+2s" "cantarás" "cantar+V+if+3s" "cantará" "cantar+V+if+1p" "cantareis" "cantar+V+if+2p" "cantarão" "cantar+V+if+3p" "cantaremos" "barco+N+ms" "barco" "barco+N+fs" "barca" "barco+N+mp" "barcos" "barco+N+fp" "barcas" "ironicamente+ADV" "ironicamente")
+#array with transducers to be tested
+array2=( "lemma2verb" "lemma2word" "word2lemma" )
+c=0
+test="test"
 for ((i=0;i<${#array[@]};++i)); do
 
-    printf "%s + %s -> %s\n" "${array[i]}" "${array2[i]}" "${array3[i]}"
+  for ((j=0;j<${#array2[@]};++j)); do
 
-    #FIXME (keep files in folder and remove this line before submission)
-    python3 word2fst.py -s syms.txt "${array[i]}" > "TXTexamples/""${array[i]}"".txt"
+      # counter and file name (test1, test2 ... and so on)
+      ((c++))
+      name=${test}${c}
 
-    #generate example.fst
-    fstcompile --isymbols=syms.txt --osymbols=syms.txt --keep_isymbols --keep_osymbols "TXTexamples/""${array[i]}"".txt" | fstarcsort > "FSTexamples/""${array3[i]}"".fst"
+      printf "%s + %s -> %s\n" "${array[i]}" "${array2[j]}" "${name}"
 
-    #generate example.pdf
-    fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait "FSTexamples/""${array3[i]}"".fst" | dot -Tpdf > "FINALexamples/""${array3[i]}"".pdf"
+      #FIXME (keep files in folder and remove this line before submission)
+      #python3 word2fst.py -s syms.txt "${array[i]}" > "TXTexamples/""${array[i]}"".txt"
 
-    #generate example_converter.fst
-    fstcompose "FSTexamples/""${array3[i]}"".fst" "FINALtransducers/""${array2[i]}"".fst" > "FSTexamples/""${array3[i]}""_""${array2[i]}"".fst"
+      #generate example.fst
+      fstcompile --isymbols=syms.txt --osymbols=syms.txt --keep_isymbols --keep_osymbols "TXTexamples/""${array[i]}"".txt" | fstarcsort > "FSTexamples/""${name}"".fst"
 
-    #generate example_converter.pdf
-    fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait "FSTexamples/""${array3[i]}""_""${array2[i]}"".fst" | dot -Tpdf > "FINALexamples/""${array3[i]}""_""${array2[i]}"".pdf"
+      #generate example.pdf
+      fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait "FSTexamples/""${name}"".fst" | dot -Tpdf > "FINALexamples/""${name}"".pdf"
+
+      #generate example_converter.fst
+      fstcompose "FSTexamples/""${name}"".fst" "FINALtransducers/""${array2[j]}"".fst" > "FSTexamples/""${name}""_""${array2[j]}"".fst"
+
+      #generate example_converter.pdf
+      fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait "FSTexamples/""${name}""_""${array2[j]}"".fst" | dot -Tpdf > "FINALexamples/""${name}""_""${array2[j]}"".pdf"
+    done
 done
